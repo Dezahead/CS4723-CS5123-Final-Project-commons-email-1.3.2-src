41f929cc76298b5c839f67a62434d11abc1df60b
Dezerel Jones <dezereljones@yahoo.com>
2020-12-04 16:22:44 -0600

    fixed Unit tests

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -2,21 +2,9 @@ package org.apache.commons.mail;
 
 import junit.framework.TestCase;
 import org.junit.Test;
-
-import javax.activation.DataHandler;
-import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMultipart;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.*;
-
-import static org.apache.commons.mail.EmailConstants.*;
-import static org.mockito.Mockito.*;
 
 public class EmailTest extends TestCase{
 	Email testEmail;
@@ -27,7 +15,7 @@ public class EmailTest extends TestCase{
 		testEmail = new SimpleEmail();
 
 		recipientList = new String[]{"MaryJones@gmail.com", "DezerelGraham@yahoo.com", "Seal@g.com", "123456@email.com"};
-		addressCollection = new ArrayList<>();
+		addressCollection = new ArrayList();
 		addressCollection.add(InternetAddress.parse("to@to.com")[0]);
 
 		testEmail.setMsg("This is a test email.");
@@ -35,11 +23,13 @@ public class EmailTest extends TestCase{
 		testEmail.setSubject("Test Subject");
 	}
 
+	@Test
 	public void testAddBcc() throws EmailException {
 		testEmail.addBcc(recipientList);
 		assertEquals("MaryJones@gmail.com", testEmail.getBccAddresses().get(0).toString());
 	}
 
+	@Test
 	public void testAddCc() throws EmailException {
 		testEmail.addCc("a@b.com");
 		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
